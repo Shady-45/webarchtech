19125f7b43e093471483bb60f228779a2fa7e0e4
Rahul <rahulchalat@yahoo.com>
2022-07-11 20:25:37 +0530

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="flex mt-[2rem] ml-[2rem] text-2xl">
+    <div className="bg-[rgba(243, 240, 250, 0.8)]">
+      <nav className="flex  mt-[2rem] ml-[2rem] text-2xl">
         <p>Web Arch</p>
         <ul className=" flex w-[80%] justify-end gap-[5.75rem]">
           <li>about</li>
